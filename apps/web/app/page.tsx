fb6c0e320455731dc3df39cea3bcf8a0d7e2d1dc
Gabriel Jesus <gabrieljesus9221@gmail.com>
2024-11-16 18:38:28 -0300

    feat: adds testing components

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 
-import "use-recaptcha-react";
+import Link from "next/link";
+import { Captcha } from "../components/LoadCaptcha";
 
 export default function Home() {
   return (
-    <div>
-      <main>This is an awesome text</main>
-    </div>
+    <main className="bg-gray-100 flex items-center justify-center min-h-screen">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-sm space-y-4">
+        <Captcha />
+        <div className="text-center">
+          <Link
+            href="/other"
+            className="text-green-500 font-semibold hover:text-green-600 transition text-center"
+          >
+            Other page
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 }
